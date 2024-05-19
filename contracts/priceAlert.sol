11773938afb9e8b6b964cc2bc3dbe3c781570675
Claudia686 <claudia_mihaela@rocketmail.com>
2024-05-19 06:48:07 +0100

    Added setThreshold function

--- contracts/priceAlert.sol
+++ contracts/priceAlert.sol
@@ -1,4 +1,24 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
-import "@chainlink/contracts/src/v0.8/interfaces/ChainlinkRequestInterface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract PriceAlert {
+    AggregatorV3Interface internal priceFeed;
+	mapping(address => uint256) public thresholds;
+
+	// Emit event when new thresholds is set
+	event ThresholdSet(address indexed user, uint256 thresholds);
+	
+	// constructor
+	constructor(address _priceFeedAddress) {
+	    priceFeed = AggregatorV3Interface(_priceFeedAddress);
+	}
+
+// Set thresholds for a user
+function setThreshold(uint256 _threshold) public {
+    thresholds[msg.sender] = _threshold; // Store the threshold in the mapping
+	emit ThresholdSet(msg.sender, _threshold); // Emit ThresholdSet event
+ }
+
+}
