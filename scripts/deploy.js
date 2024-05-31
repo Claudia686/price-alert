332dfdeb883c69a63c7c1bb74ae4d7a8b8206092
Claudia686 <claudia_mihaela@rocketmail.com>
2024-05-31 07:09:21 +0100

    Added README.md documentation plus UI setup

---
+++ scripts/deploy.js
@@ -0,0 +1,19 @@
+const hre = require("hardhat");
+
+async function main() {
+	// Setup accounts & variables
+	[deployer, user] = await ethers.getSigners()
+	const priceFeedAddress = '0xc7de7f4d4C9c991fF62a07D18b3E31e349833A18'
+
+	// Deploy the contract
+	const PriceAlert = await ethers.getContractFactory('PriceAlert');
+	const priceAlert = await PriceAlert.deploy(priceFeedAddress)	
+	// await priceAlert.deployed()
+
+	console.log(`priceAlert deployed to: ${await priceAlert.getAddress()}`)
+}
+
+main().catch((error) => {
+	console.error(error);
+	process.exitCode = 1;
+})
--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import logo from './logo.svg';
+import {  useEffect, useState } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// Abis
+import PriceAlert from './abis/price-alert.json'
 
-        
+//Config
+import config from './config.json'
+
+
+function App() {
+// Define data
+    
+}
+   return (
+}
+       
 export default App;
+
